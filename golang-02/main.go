619624d5369fec2ee0ccf7c7fc95fefc11c09748
Ali Karakoç <bykrkc@gmail.com>
2022-07-04 21:53:31 +0300

    json param example

--- golang-02/main.go
+++ golang-02/main.go
@@ -12,15 +12,28 @@ func mainHandler(c echo.Context) error {
 }
 func userHandler(c echo.Context) error {
 
+	dataType := c.Param("data")
 	username := c.QueryParam("username")
 	name := c.QueryParam("name")
 	surname := c.QueryParam("surname")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Username %s, Name: %s, Surname: %s", username, name, surname))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"username": username,
+			"name":     name,
+			"surname":  surname,
+		})
+	}
 
 	fmt.Println(username)
 	fmt.Println(name)
 	fmt.Println(surname)
 
-	return c.String(http.StatusOK, "User endpointine GET isteği yapıldı.")
+	return c.String(http.StatusBadRequest, "Yalnızca JSON veya String parametresini kullanabilirsiniz.")
 }
 func main() {
 	fmt.Printf("Hello World!")
@@ -28,7 +41,7 @@ func main() {
 	e := echo.New()
 
 	e.GET("/main", mainHandler)
-	e.GET("/user", userHandler)
+	e.GET("/user/:data", userHandler)
 
 	e.Start(":8000")
 }
