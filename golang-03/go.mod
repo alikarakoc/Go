408ea03357436cdf9a1923c588859a06c40b17c8
Ali Karakoç <bykrkc@gmail.com>
2022-07-08 23:18:38 +0300

    microservices

---
+++ golang-03/go.mod
@@ -0,0 +1,5 @@
+module golang-microservices
+
+go 1.18
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ golang-03/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+	})
+
+	http.ListenAndServe(":9090", nil)
+
+}
