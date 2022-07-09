8fe6fa4fb86b6a461c0ac4b4513fa4c1a5dfe2f7
Ali Karakoç <bykrkc@gmail.com>
2022-07-09 23:08:52 +0300

    Update main.go

--- golang-03/main.go
+++ golang-03/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,6 +11,16 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Data %s", d)
+	})
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World")
 	})
 
 	http.ListenAndServe(":9090", nil)
