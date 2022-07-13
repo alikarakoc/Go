42a5b87961616fde4faf99bb92d10e6110f7876d
Ali Karakoç <bykrkc@gmail.com>
2022-07-13 23:43:33 +0300

    form post

--- golang-04/main.go
+++ golang-04/main.go
@@ -7,10 +7,27 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supperted", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
 
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
